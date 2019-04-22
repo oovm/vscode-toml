d5c90f910c381e6cf8e93a2b97cbf531143ab7aa
Aster <galaster@foxmail.com>
2019-04-22 13:15:36 +0800

    🛒 Support comment

---
+++ source/first-class/comment.ts
@@ -0,0 +1,12 @@
+export const comment: any = {
+    captures: {
+        1: {
+            name: 'comment.line.number-sign.toml',
+        },
+        2: {
+            name: 'punctuation.definition.comment.toml',
+        },
+    },
+    comment: 'Comments',
+    match: '\\s*((#).*)$',
+}
