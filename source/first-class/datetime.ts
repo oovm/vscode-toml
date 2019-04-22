322993ce11f4f76317be776edfade371b599f6ad
Aster <galaster@foxmail.com>
2019-04-22 14:23:52 +0800

    🛒 Support datetime

---
+++ source/first-class/datetime.ts
@@ -0,0 +1,44 @@
+const local_time = {
+    captures: {
+        1: {
+            name: 'constant.other.time.toml',
+        },
+    },
+    match: '(\\d{2}:\\d{2}:\\d{2}(?:\\.\\d+)?)',
+}
+
+const local_date = {
+    captures: {
+        1: {
+            name: 'constant.other.date.toml',
+        },
+    },
+    match: '(\\d{4}\\-\\d{2}\\-\\d{2})',
+}
+
+const local_date_time = {
+    captures: {
+        1: {
+            name: 'constant.other.datetime.local.toml',
+        },
+    },
+    match: '(\\d{4}\\-\\d{2}\\-\\d{2}T\\d{2}:\\d{2}:\\d{2}(?:\\.\\d+)?)',
+}
+
+const offset_date_time = {
+    captures: {
+        1: {
+            name: 'constant.other.datetime.offset.toml',
+        },
+    },
+    match: '(?<!\\w)(\\d{4}\\-\\d{2}\\-\\d{2}[T| ]\\d{2}:\\d{2}:\\d{2}(?:\\.\\d+)?(?:Z|[\\+\\-]\\d{2}:\\d{2}))(?!\\w)',
+}
+
+//ordered, must reverse
+export const datetime: any = [
+    offset_date_time,
+    local_date_time,
+    local_date,
+    local_time,
+]
+
